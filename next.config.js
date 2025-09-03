1b4cec1f4e6a8e3730f23cb030148049b8271e86
Yahaonchik <Danilqa123456@gmail.com>
2025-09-03 23:15:54 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,4 +2,21 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        fs: false,
+        path: false,
+        stream: false,
+        zlib: false,
+      }
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        'gzip-size': 'gzip-size/browser',
+      }
+    }
+    return config
+  },
+});
