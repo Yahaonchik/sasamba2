9bd65f239eb3d859a7476182298a013d51a31f5a
Yahaonchik <Danilqa123456@gmail.com>
2025-09-04 12:14:32 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,11 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  images: {
-    // Extend responsive widths to allow larger, crisper renders on big/retina screens
-    deviceSizes: [320, 360, 375, 414, 640, 750, 828, 1080, 1200, 1536, 1920, 2048, 2560, 3840],
-    imageSizes: [149, 215, 300, 450, 600],
-    formats: ['image/webp'],
-  },
-});
+module.exports = withBundleAnalyzer({});
